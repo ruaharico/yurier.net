cadc0345cf9faeb21de19a578107bd87671a9240
ruaharico <58129639+ruaharico@users.noreply.github.com>
2023-10-22 17:18:00 +0800

    更新 nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -2,6 +2,5 @@ export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
   { href: '/projects', text: '项目' },
-  { href: '/guestbook', text: '留言墙' },
   { href: '/about', text: '关于' },
 ]

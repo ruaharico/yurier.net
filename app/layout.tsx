8ec0be451498494c0a70ef1d3c491db73ca91c01
arcHarico <harico@yurier.net>
2023-10-22 19:32:12 +0800

    优化

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     default: seo.title,
   },
   description: seo.description,
-  keywords: '百合研社团,Yurier Department,百合研Yurier,百合研,Yurier,百合社团',
+  keywords: '百合研社团,Yurier Department,百合研Yurier,百合研,Yurier,百合社团,百合漫画,百合动漫,百合动画,百合小说,百合游戏,百合音声,百合研究,百合安利,百合音乐,百合视频,百合文章,百合专栏,百合论坛,百合贴吧,百合吧,反百合吧,反百合控吧,新百合吧,百合小说吧,百合会新站,百合会论坛,300,300新站,300论坛',
   themeColor: [
     { media: '(prefers-color-scheme: dark)', color: '#000212' },
     { media: '(prefers-color-scheme: light)', color: '#fafafa' },

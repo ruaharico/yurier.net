e44718712de696aa460cee4b226420001bfd65a5
ruaharico <58129639+ruaharico@users.noreply.github.com>
2023-10-22 16:48:58 +0800

    更新 Projects.tsx

--- app/(main)/projects/Projects.tsx
+++ app/(main)/projects/Projects.tsx
@@ -10,8 +10,8 @@ export async function Projects() {
       className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
     >
       {projects.map((project) => (
-        <ProjectCard project={project} key={project._id} />
+        <ProjectCard project={Empty} key={Test} />
       ))}
-    </ul> : <strong>🔥 产品和项目很多，待我慢慢更新！</strong>
+    </ul>
   )
 }

37651fd035395dfda49f9d2797ccda1eba771be5
arcHarico <harico@yurier.net>
2023-11-17 14:08:57 +0800

    Feat

--- app/(main)/about_en/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '~/components/ui/Container'
-
-export default function AboutPage() {
-  return (
-    <Container>
-      Loading
-    </Container>
-  )
-}
--- app/(main)/blog/BlogPostPage.tsx
+++ app/(main)/blog/BlogPostPage.tsx
@@ -13,7 +13,7 @@ import {
   CursorClickIcon,
   HourglassIcon,
   ScriptIcon,
-  UTurnLeftIcon,
+  HomeIcon,
 } from '~/assets'
 import { ClientOnly } from '~/components/ClientOnly'
 import { PostPortableText } from '~/components/PostPortableText'
@@ -44,12 +44,12 @@ export function BlogPostPage({
         </aside>
         <div className="max-w-2xl md:flex-1 md:shrink-0">
           <Button
-            href="/blog"
+            href="/"
             variant="secondary"
-            aria-label="返回「公告与成果聚合」"
+            aria-label="返回主页"
             className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
           >
-            <UTurnLeftIcon className="h-8 w-8 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+            <HomeIcon className="h-8 w-8 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
           </Button>
           <article data-postid={post._id}>
             <header className="relative flex flex-col items-center pb-5 after:absolute after:-bottom-1 after:block after:h-px after:w-full after:rounded after:bg-gradient-to-r after:from-zinc-400/20 after:via-zinc-200/10 after:to-transparent dark:after:from-zinc-600/20 dark:after:via-zinc-700/10">
